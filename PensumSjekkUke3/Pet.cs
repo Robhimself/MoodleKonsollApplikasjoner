c9d61a6e5f500c20ac9674f82c0d9ec2e194c05b
Robert Lysebo <robertlysebo@gmail.com>
2023-01-23 15:00:43 +0100

    Pensum check

---
+++ PensumSjekkUke3/Pet.cs
@@ -0,0 +1,44 @@
+﻿namespace PensumSjekkUke3;
+
+public class Pet
+{
+    public string Name { get; private set; }
+    public int Age { get; }
+    public string Color { get; }
+
+    public static int Counter;
+
+    public Pet(string name, int age, string color)
+    {
+        Name = name;
+        Age = age;
+        Color = color;
+        Counter++;
+    }
+
+    public void Feed(string? food)
+    {
+        string reply;
+        if (food.Length < 2)
+        {
+            reply = $"{Name}: \"That's not real food!\"";
+            Console.WriteLine(reply);
+        }
+        else
+        {
+            reply = $"{Name}: \"Mmm... {food}. Thank you, I was very hungry.\"";
+            Console.WriteLine(reply);
+        }
+    }
+    public void Feed(string? food, bool favorite)
+    {
+        string reply;
+        if (!favorite)
+        {
+            Feed(food);
+            return;
+        }
+            reply = $"{Name}: \"Mmm... {food}. That's my favorite! How did you know? Thank you very much!\"";
+            Console.WriteLine(reply);
+    }
+}
---
+++ PensumSjekkUke3/Program.cs
@@ -0,0 +1,24 @@
+﻿using PensumSjekkUke3;
+
+/*  Lag en applikasjon som kan generere opp flere forskjellige virtuelle kjæledyr. 
+    
+    1. Bruk constructor, skal ikke kunne endre navn, age eller andre characteristics utenfra klassen.
+    
+    2. Lag funksjonalitet som gjør at man kan gi dyret mat, 
+    bruk en overload til å gi en annen feedback til console dersom det er favorittmaten til dyret 
+    - feks. "Hurra dette er det beste jeg vet, tusen takk for maten!" istedenfor feks. "Takk for maten".
+*/
+
+var hest = new Pet("Harald", 1, "Brun");
+Console.WriteLine($"Din nye hest heter: {hest.Name}.");
+Console.WriteLine(Pet.Counter);
+
+var katt = new Pet("Fredrik", 2, "Hvit");
+Console.WriteLine($"Din nye hest heter: {hest.Name}.");
+Console.WriteLine(Pet.Counter);
+
+hest.Feed("Høy", true);
+
+hest.Feed("Brunost");
+
+hest.Feed("x");
