1937d6fae83e4f0ff29c80430944498dc749f6f0
Robert Lysebo <robertlysebo@gmail.com>
2023-02-06 12:50:39 +0100

    Bossfight oppgave lagt til.

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,68 @@
+﻿namespace Bossfight;
+
+public class GameCharacter
+{
+    public string Name { get; }
+    public int Health { get; private set; }
+    public int Stamina { get; }
+    private int Strength { get; }
+    private int MinStrength { get; }
+    private bool IsStrengthRandom { get; }
+
+    public GameCharacter(string name, int health, int stamina, int strength)
+    {
+        Name = name;
+        Health = health;
+        Stamina = stamina;
+        Strength = strength;
+    }
+    public GameCharacter(string name, int health, int stamina, int minStrength, int maxStrength)
+    {
+        Name = name;
+        Health = health;
+        Stamina = stamina;
+        Strength = maxStrength;
+        MinStrength = minStrength;
+        IsStrengthRandom = true;
+    }
+
+    public void Fight(GameCharacter opponent)
+    {
+        int damage;
+        
+        
+        if (IsStrengthRandom)
+        {
+            var rand = new Random();
+            damage = rand.Next(MinStrength, Strength + 1);
+        }
+        else
+        {
+            damage = Strength;
+        }
+
+        opponent.Health -= damage;
+        
+        if (opponent.Health < 0) opponent.Health = 0;
+        
+        Console.WriteLine($"{Name} attacked {opponent.Name} and inflicted {damage} damage. {opponent.Name} now has {opponent.Health} health left. ");
+    }
+
+    public bool Recharge(int currentStamina)
+    {
+        var status = $"{currentStamina}/{Stamina}";
+        
+        if (currentStamina < Stamina)
+        {
+            Console.WriteLine($"{Name}'s stamina is low ({status}).");
+            Console.WriteLine($"{Name} can't fight until stamina is full... *** Stamina +10 ***");
+            
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"{Name}: Stamina is now full! I can fight again next round!");
+            return false;
+        }
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using Bossfight;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is BossFight!");
+Console.WriteLine();
+
+var hero = new GameCharacter("Hero",100, 40, 20);
+var boss = new GameCharacter("Boss",400, 10, 0, 30);
+
+var currentHeroStamina = hero.Stamina;
+var currentBossStamina = boss.Stamina;
+var heroIsRecharging = false;
+var bossIsRecharging = false;
+
+while (hero.Health > 0 && boss.Health > 0)
+{
+    if (heroIsRecharging == false)
+    {
+        if (currentHeroStamina <= 0)
+        {
+            heroIsRecharging = hero.Recharge(currentHeroStamina);
+            currentHeroStamina += 10;
+        }
+        else
+        {
+            hero.Fight(boss);
+            currentHeroStamina -= 10;
+        }
+    }
+    else
+    {
+        heroIsRecharging = hero.Recharge(currentHeroStamina);
+        currentHeroStamina += 10;
+    }
+
+    if (bossIsRecharging == false)
+    {
+        if (currentBossStamina <= 0)
+        {
+            bossIsRecharging = boss.Recharge(currentBossStamina);
+            currentBossStamina += 10;
+        }
+        else
+        {
+            boss.Fight(hero);
+            currentBossStamina -= 10;
+        }
+    }
+    else
+    {
+        bossIsRecharging = boss.Recharge(currentBossStamina);
+        currentBossStamina += 10;
+    } 
+}
+
+Console.WriteLine(hero.Health <= 0 // Resharper Magic: refactored from if-else statement. 
+    ? $"{boss.Name} won! {hero.Name} has fallen..."
+    : $"{hero.Name} has defeated {boss.Name}! {hero.Name} is our savior! Long live {hero.Name}!");
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
